127c5f4d77f5d0c4a339e932de24d67afdb72994
atulmahindra <jadhav.atul3mahindralogistics.com>
2025-10-13 15:52:23 +0530

    template added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,16 @@ import { BackendHeaderComponent } from './backend-header/backend-header.componen
 import { BackendFooterComponent } from './backend-footer/backend-footer.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { SharedService } from './shared.service';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
 
 
 @NgModule({
@@ -22,10 +32,18 @@ import { SharedService } from './shared.service';
     HeaderComponent,
     FooterComponent,
     BackendHeaderComponent,
-    BackendFooterComponent
+    FlexLayoutModule,
+    BackendFooterComponent,
+    SideNavComponent
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule,
     SharedRoutingModule
   ],
   providers: [SharedService]
