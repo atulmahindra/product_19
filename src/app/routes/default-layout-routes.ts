d96dceb5ffa56bb1992149b19bd75979db24ae8a
atulunitech <atulunitech@gmail.com>
2025-10-17 01:52:16 +0530

    routing change

--- src/app/routes/default-layout-routes.ts
+++ src/app/routes/default-layout-routes.ts
@@ -5,9 +5,9 @@ export const DEFAULT_ROUTES: Routes = [
     {
         path: '',
         loadChildren: () => import('../cms/cms.module').then(m => m.CmsModule)
-    },
-    {
-        path: 'auth',
-        loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule)
     }
+    // {
+    //     path: '',
+    //     loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule)
+    // }
 ]
