fc16999608d79dae8de1b055bf46755bee2e2a02
atulunitech <atulunitech@gmail.com>
2025-10-20 15:36:07 +0530

    update git

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     CommonModule,
     AuthRoutingModule
-  ]
+  ],
+  providers: [AuthService]
 })
 export class AuthModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   standalone: false,
@@ -7,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+constructor( private router: Router,private _authService: AuthService) {
+    
 
+   }
 }
