3a6dd5b0570fef95cc95c9b2bba0078c3b4a1a41
atulmahindra <jadhav.atul3mahindralogistics.com>
2025-11-10 10:06:36 +0530

    changes done

--- src/environments/environment.qa.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    API_URL: 'https://logioneapi.mllqa.com/',
-    APP_URL:"https://logione.mllqa.com/",
-    secretKey: "mllf3xpex2kPk",
-    ssoLogin: 'https://login.mllqa.com/sso/login?client_id=logione'
-  };
--- src/environments/environment.stage.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    API_URL: 'https://logioneapi.whizzard.in/',
-    secretKey: "mllf3xpex2kPk",
-  };
-  
